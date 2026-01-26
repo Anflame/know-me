78fc94f5da7aac7f9138a3110c11646fabcf474e
Anflame <anflame@mail.ru>
2026-01-26 23:52:32 +0400

    testes in progress

--- src/pages/api/mentors/index.ts
+++ src/pages/api/mentors/index.ts
@@ -1,13 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getMentors, getMentorsWithParams } from '@/server/services/mentors';
 import { FilterParams } from '@/server/types/mentors';
+import { isEmptyObject } from '@/utils/object';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
 
-  if (req.query) {
+  if (!isEmptyObject(req.query)) {
     const mentors = await getMentorsWithParams(req.query as FilterParams);
 
     return res.status(200).json(mentors);
