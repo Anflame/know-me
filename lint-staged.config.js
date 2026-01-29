fed3cc9fc6212ecf86bcfad1ec59128a509edf5c
Anflame <anflame@mail.ru>
2026-01-29 10:38:39 +0400

    fix staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{ts,tsx}': () => ['npm run lint --fix', 'npm run check-types', 'npm run test-coverage'],
+  '*.{ts,tsx}': () => ['npm run lint --fix', 'npm run test -- --findRelatedTests'],
   '*.{js,json,md}': () => 'npm run format',
 };
